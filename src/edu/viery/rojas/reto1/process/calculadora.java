0c98a4a406d2a44b43765d8d94203faa712bcf3f
VieryDanielRojasHernandezx <123215273+VieryDanielRojasHernandezx@users.noreply.github.com>
2023-01-28 12:19:30 -0600

    Create calculadora.java

---
+++ src/edu/viery/rojas/reto1/process/calculadora.java
@@ -0,0 +1,8 @@
+package edu.viery.rojas.reto1.process;
+
+public class calculadora {
+    public static int sumarNumeros(int operando1,int operando2) {
+        return operando1 + operando2;
+    }
+}
+
